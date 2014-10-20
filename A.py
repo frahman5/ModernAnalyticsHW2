ee0b055bd02036bcb44f3a0ddbc04af438b8863f
Faiyam Rahman <frahman305@gmail.com>
2014-10-20 17:47:59 -0400

    wrote skeletons for A.py

---
+++ A.py
@@ -0,0 +1,21 @@
+__author__ = 'Faiyam Rahman, Rachel Mayer'
+
+def make3DHist(dlongbinsize, dlatbinsize, data):
+    """
+    float float np.array -> ???
+
+    Given the parameters for binsize and a matrix of data, 
+    produces (saves to file) a 3DHistogram
+    """
+    pass
+    
+def main(trainData):
+    """
+    string -> None
+
+    Given the filepath of training data of taxi trips in NYC, produces
+    plots of the pdfs of 
+        P(passenger count=1|dropoff longitude, dropoff latitude)
+        P(passenger count=3|dropoff longitude, dropoff latitude)
+    """
+    pass
---
+++ C.py
@@ -0,0 +1,3 @@
+__author__ = 'Faiyam Rahman, Rachel Mayer'
+
+

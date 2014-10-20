c497bd8dcbe91e50a2316beff1763d34db2d37a8
rachelita <rmayer3@gmail.com>
2014-10-20 18:10:30 -0400

    added plot funct

--- A.py
+++ A.py
@@ -7,7 +7,24 @@ def make3DHist(dlongbinsize, dlatbinsize, data):
     Given the parameters for binsize and a matrix of data, 
     produces (saves to file) a 3DHistogram
     """
-    pass
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111, projection='3d')
+
+    xpos = [1,2,3,4,5,6,7,8,9,10]
+    ypos = [2,3,4,5,1,6,2,1,7,2]
+    num_elements = len(xpos)
+    zpos = [0,0,0,0,0,0,0,0,0,0]
+    dx = np.ones(10)
+    dy = np.ones(10)
+    dz = [1,2,3,4,5,6,7,8,9,10]
+
+    ax1.bar3d(xpos, ypos, zpos, dx, dy, dz, color='#00ceaa')
+    
+    plt.savefig(foo)
     
 def main(trainData):
     """

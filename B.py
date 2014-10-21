588ccc8e174f1b6e9d91e6bc7393399f6a6835f0
Faiyam Rahman <frahman305@gmail.com>
2014-10-21 12:11:01 -0400

    added code signatures for B

--- B.py
+++ B.py
@@ -1,1 +1,22 @@
-__author__ = 'aub3'
+__author__ = 'Faiyam Rahman, Rachel Mayer'
+
+def euclideanDistance(vector1, vector2):
+    """
+    TupleOfFloats TupleOfFloats -> float
+
+    Given two length-n vectors, returns the euclidean distance between them
+    """
+    pass
+
+def main():
+    """
+    Using 1 nearest neighbor, predicts NYC Taxi trip times based on feature 
+    vectors (pickup latitude, pickup longitude, dropoff latitude, dropoff latitude). 
+
+    Tests on a subset of trip_data_1.csv
+    """
+    pass
+
+# Usage: python B.py
+if __name__ == '__main__':
+    main()

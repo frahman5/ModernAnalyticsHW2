a9d1b8ea2ce4c23a73f307cf58e061bda08856e8
Faiyam Rahman <frahman305@gmail.com>
2014-10-28 23:33:11 -0400

    decision tree is now functional on very small data

--- problem2Framework/decision_tree.py
+++ problem2Framework/decision_tree.py
@@ -1,56 +1,150 @@
 import scan
-import util
+from utils import entropy, informationGain, freq
+import scipy.sparse as ss
 
+## TODO: scroll all the way down
+## -> Test the code with a small set (maybe 5) of real reviews
+## make scan.scan run fast enough
+## run it and be done!
 class DecisionTree(object):
+
     node_label = None  # takes the values 0, 1, None. If has the values 0 or 1, then this is a leaf
     left = None
     right = None
+    decision_word = None
 
     def decision(self, data):
-        raise 'not defined'
+        assert self.decision_word is not None, "If node is making a decision, should have a decision word"
+        if self.decision_word in data:
+            return self.right.go(data)
+        else:
+            return self.left.go(data)
 
     def go(self, data):
-        if node_label != None:
-            return node_label
-        return go(decision(data))
+        print "decision word: {}".format(self.decision_word)
+        if self.node_label is not None:
+            return self.node_label  
+        return self.decision(data)
 
-# http://en.wikipedia.org/wiki/ID3_algorithm
-def train(P, N, training_reviews):
+#http://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.lil_matrix.html#scipy.sparse.lil_matrix
+def makeReviewMatrix(PN, training_data):
     """
-    ListOfStrings ListOfStrings ListOfTuples -> DecisionTree
-        P: 500 Most frequent unigrams in positive reviews
-        N: 500 Most frequent unigrams in negative reviews
-        training_reviews: (review, score) duples where
+    ListOfStrings ListOfTuples -> scipy.sparse.csr_matrix
+        PN: 500-1000 Most frequent unigrams in positive and negative reviews (
+            union of 500 most frequent positive and 500 most frequent negative
+            unigrams)        
+        training_data: 
+            a listOfTuples with (review, score) duples where
+            - review is a tuple of unigrams (strings)
+            - score is either 1 or 0. 1 means positive, 0 if negative
+
+    returns a scipy.sparse.csr_matrix with structure as defined in "train"
+    """
+    import numpy as np
+    num_reviews = len(training_data)                            # num columns
+    num_words = len(PN)                                         # num rows - 1
+
+    ## Construct using a row-based linked list sparse matrix
+        # initalize a linked list row-based matrix of size num_reviews x num_words + 1
+    lil_matrix = ss.lil_matrix((num_reviews, num_words + 1), dtype=float)
+        # add a row at a time
+    for index, datum in enumerate(training_data):
+        review, score = datum
+        frequencyDict, total_words = freq(review)     
+            # a_ij is the numbe rof times word j appears in review i, 0 <=j <= n
+        counts = [frequencyDict[word] if word in frequencyDict else 0 for word in PN]
+            # a_ij is 1 if the review is positive, 0 if negative, j = n+1
+        counts.append(score)
+            # replace the corresponding index
+        lil_matrix[index, :] = np.array([counts])
+    
+    return lil_matrix.tocsr()
+
+    
+# http://en.wikipedia.org/wiki/ID3_algorithm
+# http://docs.scipy.org/doc/scipy-0.14.0/reference/sparse.html
+def train(PN, training_data):
+    """
+    ListOfStrings ListOfTuples|scipy.sparse.csr_matrix -> DecisionTree
+        PN: 500-1000 Most frequent unigrams in positive and negative reviews (
+            union of 500 most frequent positive and 500 most frequent negative
+            unigrams)
+        training_data: 
+            Either a listOfTuples with (review, score) duples where
             - review is a set of unigrams (strings)
             - score is either 1 or 0. 1 means positive, 0 if negative
+
+            Or a m x n+1 scipy.sparse.csc_matrix with:
+                Rows 1-m corresponding to reviews.
+                Columns 1-n corresponding to words in PN. Column
+                n+1 is 1|0 depending on whether or not review n is positive or negative
+
+                a_ij = number of occurences of word j in review i (i<=n, j<=m)
+                a_ij = 1 if review i is positive, 0 if review i is negative (i=n+1, j<=m)
+                a_ij = 0 otherwise
 
     Returns a decision Tree based on the ID3 algorithm to determine whether a 
     review is positive or negative
     """
     tree = DecisionTree()
 
+    # Construct a sparse matrix if necessary
+    if type(training_data) == list:
+        training_data = makeReviewMatrix(PN, training_data)
+
     ## Base Case
-    If all reviews are same class (e.g all positive):
-        tree.node_label = that class (e.g positive)
+    num_positive_reviews = training_data.sum(axis=0)[0,-1]   # sum of entries in far right column
+    num_reviews = training_data.get_shape()[0]
+    if num_positive_reviews == num_reviews:                  # all reviews positive
+        tree.node_label = 1
+        return tree
+    elif num_positive_reviews == 0:                          # all reviews negative
+        tree.node_label = 0
         return tree
 
     ## Recursive Case
-    set_of_unigrams_and_frequencies = {(word, count)| words in (P union N), 
-                                        and count is total number of occurences
-                                        of the word in all the reviews}
-        # Calculate maximum information gain
+        # Determine which word offers maximum information gain
+    # import pdb
+    # pdb.set_trace()
     maxGain = 0
-    decisionWord = None
-    for word in (N union P):
-        x = informationGain(word, set_of_unigrams_and_frequencies)
-        if x > maxGain:
-            maxGain = x
-            decisionWord = word
+    left_tree, right_tree, decision_word = None, None, None
+    left_column_indices, right_column_indices = None, None
+    for index, word in enumerate(PN):
+        ig_x, t1, t2, pn1_indices, pn2_indices = informationGain(index, training_data)
+        if ig_x > maxGain:
+            maxGain = ig_x
+            left_tree = t1
+            right_tree = t2
+            decision_word = word
+            left_column_indices = pn1_indices
+            right_column_indices = pn2_indices
 
-        # make appropriate decision 
-    nowTree.left = train(P, N, all reviews without decisionWord)
-    nowTree.right = train(P, N, all reviews with decisionWord)
+    assert left_tree is not None, "we should have a left tree!"
+    assert right_tree is not None, "we should have a right tree!"
+
+        # make appropriate decision
+    tree.decision_word = decision_word 
+    tree.left = train((word for index, word in enumerate(PN) if index in left_column_indices),
+                      left_tree)
+    tree.right = train((word for index, word in enumerate(PN) if index in right_column_indices),
+                       right_tree)
+
+    
+
+    return tree
 
 def test(data):
     raise 'not implemented'
 
+if __name__ == '__main__':
+    PN = ['good', 'bad', 'heyyo!']
+    training_data = [(('this', 'product', 'is', 'good'), 1),
+                     (('this', 'product', 'is', 'bad'), 0), 
+                     (('this', 'is', 'ok,', 'heyyo!'), 1)]
+
+    decision_tree = train(PN, training_data)
+    print decision_tree.go(('this', 'is', 'ok,', 'heyyo!'))
+    print decision_tree.go(('this', 'product', 'is', 'bad'))
+    print decision_tree.go(('this', 'product', 'is', 'good'))
+
+    # INSERT CODE To RUN makeReviewMatrix
--- problem2Framework/utils.py
+++ problem2Framework/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # decision tree stuff
 def entropy(frequencies):
     """
@@ -15,9 +17,85 @@ def entropy(frequencies):
         entropy -= frequency * math.log(frequency, 10)
     return entropy
 
+def calc_entropy_of_matrix(sparse_matrix):
+    """
+    Helper function for informationGain
+    """
+    column_sums = sparse_matrix.sum(axis=0)[0, :-1]
+    total = column_sums.sum(axis=1)[0, 0]
+    frequencies = tuple(np.array(column_sums/total)[0])
 
-def information_gain():
-    raise 'not implemented'
+    return entropy(frequencies)
+
+def removeZeroColumnsOfMatrix(sparse_matrix, nonremovable):
+    """
+    scipy.sparse.sparsematrix int -> scipy.sparse.sparsematrix ListOfInts
+
+    Helper function for informationGain
+    """
+    nonzero_indices = np.nonzero(sparse_matrix)
+    non_zero_column_indices = list(nonzero_indices[1])       
+    unique_nonzero_columns = sorted(set(non_zero_column_indices))
+    if nonremovable not in unique_nonzero_columns:                 # to gaurd against removing last column
+        unique_nonzero_columns.append(nonremovable) 
+
+    return sparse_matrix[:, unique_nonzero_columns], unique_nonzero_columns
+
+def informationGain(j, training_data):
+    """
+    int scipy.sparse.csr_matrix -> float scipy.sparse.csr_matrix scipy.sparse.csr_matrix tuple tuple
+
+    Calculates the information gain of word j with respect to the set represented
+    by training_data. See decision_tree.train for the structure of the matrix. 
+    Returns the gain, the "t1" matrix, the "t2" matrix, and tuples of ints
+    indicating which columns were removed from t1 and t2 respectively. 
+
+    IG(word, training_data) = Entropy(S) - p(t1)Entropy(t1) - p(t2)Entropy(t2)
+    where S is the training data and
+    where t1 is only the rows in training_data with 0 in column word
+          and t2 is the only the rows in training_data with 1 in column word
+    """
+    import scipy.sparse as ss
+
+    ## Calculate Entropy(S)
+    entropy_s = calc_entropy_of_matrix(training_data)
+
+    ## Calculate p(t1), Entropy(t1), p(t2), Entropy(t2)
+    col_word = training_data.getcol(j)
+    total_num_reviews, total_num_cols = training_data.get_shape()
+    num_reviews_with_nonzero_val = col_word.nnz
+    num_reviews_with_zero_val = total_num_reviews - num_reviews_with_nonzero_val
+
+    # Make t1 and t2
+    t1 = ss.lil_matrix((num_reviews_with_zero_val, total_num_cols), dtype='float')
+    t2 = ss.lil_matrix((num_reviews_with_nonzero_val, total_num_cols), dtype='float')
+    col_word_generator = (elem[0] for elem in col_word.toarray())
+    t1_row_index, t2_row_index = 0, 0
+    for master_row_index, word_count in enumerate(col_word_generator):
+        if word_count == 0:
+            t1[t1_row_index, :] = training_data[master_row_index, :]
+            t1_row_index += 1
+        elif word_count != 0:
+            t2[t2_row_index, :] = training_data[master_row_index, :]
+            t2_row_index += 1
+        else:
+            raise "should not happen!"
+    
+    # Calculate p(t1), p(t2)
+    p_t1 = float(num_reviews_with_zero_val)/total_num_reviews
+    p_t2 = float(num_reviews_with_nonzero_val)/total_num_reviews
+
+    # Calculate entropy(t1), entropy(t2)
+    entropy_t1 = calc_entropy_of_matrix(t1)
+    entropy_t2 = calc_entropy_of_matrix(t2)
+
+    ## Calculate information gain, prune t1 and t2,  and return them
+    information_gain = entropy_s - (p_t1 * entropy_t1) - (p_t2 * entropy_t2)
+    # import pdb
+    # pdb.set_trace()
+    t1, preserved_columns_1 = removeZeroColumnsOfMatrix(t1, nonremovable=total_num_cols-1)
+    t2, preserved_columns_2 = removeZeroColumnsOfMatrix(t2, nonremovable=total_num_cols-1)
+    return information_gain, t1.tocsr(), t2.tocsr(), preserved_columns_1, preserved_columns_2
 
 # natural language processing stuff
 def freq(reviewAsListOfStrings):
@@ -25,7 +103,7 @@ def freq(reviewAsListOfStrings):
     ListOfStrings -> (Dictionary, int)
         reviewAsListOfStrings: e.g ['this', 'product', 'stinks', 'this', 'sucks!']
 
-    Given a review as a list of strings, a tuple with entries:
+    Given a review as a list of strings, outputs a tuple with entries:
         1) Dictionary with key, value pairs (string, frequency) e.g ('this', 2)
         2) The number of words in the input list as an int (e.g 5)
     """
